80eed9cc1e3bc456e2c6556e1dfa1cd6e3148ab8
Philippe Vo <philippe.vo.nam@gmail.com>
2026-06-11 15:50:42 -0400

    added real unit test example

---
+++ tests/test_example.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestCameraInterface(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        """called only once, before any test in the class"""
+        pass
+
+    @classmethod
+    def tearDown(cls):
+        """called only once, after any test in the class"""
+        pass
+
+    def test_save_frame(self):
+        pass
